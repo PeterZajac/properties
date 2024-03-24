2e688faa358d953494aaa82233bf56ceb42ff26b
PeterZajac <zajacp011@gmail.com>
2024-03-24 23:14:17 +0100

    Refactor PropertiesPage component and update package dependencies

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import properties from "@/properties.json";
+import PropertyCard from "@/components/PropertyCard";
 
-const PropertiesPsge = () => {
-  return <div>PropertiesPsge</div>;
+const PropertiesPage = () => {
+  return (
+    <section class="px-4 py-6">
+      {properties.length === 0 ? (
+        <p>no properites found</p>
+      ) : (
+        <div class="container-xl lg:container m-auto px-4 py-6">
+          <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property) => {
+              return <PropertyCard key={property._id} property={property} />;
+            })}
+          </div>
+        </div>
+      )}
+    </section>
+  );
 };
 
-export default PropertiesPsge;
+export default PropertiesPage;
